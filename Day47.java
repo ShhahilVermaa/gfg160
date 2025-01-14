5381d86d1cef5a37ea91ae267f76c82cba518aab
Shahil <shahiljsr2503@gmail.com>
2025-01-14 06:05:45 +0530

    Equilibrium Point

---
+++ Day47.java
@@ -0,0 +1,27 @@
+//Given an array arr[] of non-negative numbers. The task is to find the first equilibrium point in the array.
+
+//The equilibrium point in an array is an index (0-based indexing) such that the sum of all elements before that index is the same as the sum of elements after it. //Return -1 if no such point exists. 
+
+class Solution {
+    // Function to find equilibrium point in the array.
+    public static int findEquilibrium(int arr[]) {
+    
+        int totalSum = 0;
+        int leftSum = 0;
+        
+        for(int num:arr)
+        {
+            totalSum += num;
+        }
+        for(int i = 0; i < arr.length; i++)
+        {
+            totalSum -= arr[i];
+            
+            if(leftSum == totalSum) return i;
+            
+            leftSum += arr[i];
+        }
+        
+        return -1;
+    }
+}
