a7312b530b78fc41ab3d892eebff9884043941e2
Shahil <shahiljsr2503@gmail.com>
2025-02-22 13:12:16 +0530

    Longest valid Parentheses

---
+++ Day86.java
@@ -0,0 +1,27 @@
+/*Given a string s consisting of opening and closing parenthesis '(' and ')'. Find the length of the longest valid parenthesis substring.
+
+A parenthesis string is valid if:
+
+For every opening parenthesis, there is a closing parenthesis.
+The closing parenthesis must be after its opening parenthesis.*/
+class Solution {
+    static int maxLength(String s) {
+        int maxLen = 0;
+        Stack<Integer> stack = new Stack<>();
+        stack.push(-1);
+        
+        for(int i = 0 ; i < s.length() ; i ++){
+            if(s.charAt(i) == '('){
+                stack.push(i);
+            }else{
+                stack.pop();
+                if(stack.isEmpty()){
+                    stack.push(i);
+                }else{
+                    maxLen = Math.max(maxLen, i - stack.peek());
+                }
+            }
+        }
+        return maxLen;
+    }
+}
